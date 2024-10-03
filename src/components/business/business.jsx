ebf4d4acec588fb51d0eb278be26df6925ca53c1
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-03 16:13:41 -0300

    business comp

---
+++ src/components/business/business.jsx
@@ -0,0 +1,27 @@
+import styles from './business.module.css';
+
+
+export default function Business() {
+
+    return (
+        <div className={styles.businessContainer}>
+            <div className={styles.business}>
+                <img 
+                    src='/img/venn-color.svg' 
+                    alt='Venn Studio'
+                    className={styles.logo}
+                />
+                <p>
+                    Software studio focused on innovative solutions to web and mobile apps, decentralized apps, smart contract protocols and other digital solutions. Consulting on app development, including blockchain and decentralized apps.
+                    <br/><br/>
+                    Founded in 2024 after a year or so of collaboration between it’s 3 founders, Pedro Franceschin, Bernardo Franceschin and Fernanda Duarte.
+                    <br/><br/>
+                    <span style={{ opacity: 0.6}}>
+                        Role:
+                    </span>
+                    Co-owner, engineer, full-stack developer.
+                </p>
+            </div>
+        </div>
+    )
+}
