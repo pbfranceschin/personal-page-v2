13d8e5153c4afc804f74329fdd9a241157901a99
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-07 18:11:45 -0300

    fix: svg props

--- src/components/graphics/graphics.jsx
+++ src/components/graphics/graphics.jsx
@@ -88,7 +88,7 @@ export const ScrollUpIcon = () => {
 
 export const ChevronDown = () => {
     return (
-        <svg  xmlns="http://www.w3.org/2000/svg"  width="100%"  height="100%"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg>
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="100%"  height="100%"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg>
     )
 }
 
