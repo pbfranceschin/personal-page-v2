0c3b798a480bc2f40c68070fc2645558448b790d
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-22 07:52:17 -0300

    layout: link icon positioning

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,5 +1,5 @@
 import styles from './portfolio.module.css';
-import { GitHubIcon, GlobeIcon } from '../graphics/graphics';
+import { GitHubIcon, ChainIcon } from '../graphics/graphics';
 import { useContext } from 'react';
 import { AppContext } from '../../context/provider';
 
@@ -19,7 +19,21 @@ export default function Portfolio() {
                         <div className={styles.nameContainer}>                
                             <a>
                                 <h3 className={styles.name}>venn protocol</h3>
-                            </a>                            
+                            </a>
+                            <div className={styles.links}>
+                            <a>
+                                <div className={styles.icon}
+                                // style={{ padding: '2px'}}
+                                >
+                                    <GitHubIcon/>                            
+                                </div>
+                            </a>
+                            <a>
+                                <div className={styles.icon}>
+                                    <ChainIcon/>
+                                </div>
+                            </a>                                
+                        </div>
                         </div>
                         <p>
                             {portfolio.entries[0].description[0]}
@@ -27,7 +41,7 @@ export default function Portfolio() {
                         <p>
                             {portfolio.credit.selfVenn}
                         </p>
-                        <div className={styles.links}>
+                        {/* <div className={styles.links}>
                             <a>
                                 <div className={styles.icon}
                                 style={{ padding: '2px'}}
@@ -37,10 +51,10 @@ export default function Portfolio() {
                             </a>
                             <a>
                                 <div className={styles.icon}>
-                                    <GlobeIcon/>
+                                    <ChainIcon/>
                                 </div>
                             </a>                                
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div className={styles.line}></div>
@@ -57,12 +71,15 @@ export default function Portfolio() {
                             <a>
                                 <h3 className={styles.name}>{portfolio.entries[1].name}</h3>
                             </a>
+                            <div className={styles.links}>
+                                <a><div className={styles.icon}><ChainIcon/></div></a>
+                            </div>
                         </div>
                         <p>{portfolio.entries[1].description}</p>
                         <p>{portfolio.credit.clientVenn1}. <span style={{ textTransform: 'capitalize'}}>{pageContent.words.project}</span> {pageContent.grammar.do}<i> Conservation Strategy Fund</i>.</p>
-                        <div className={styles.links}>
-                            <a><div className={styles.icon}><GlobeIcon/></div></a>
-                        </div>
+                        {/* <div className={styles.links}>
+                            <a><div className={styles.icon}><ChainIcon/></div></a>
+                        </div> */}
                     </div>                    
                 </div>
                 <div className={styles.line}></div>
@@ -76,27 +93,27 @@ export default function Portfolio() {
                 <div className={styles.textbox}>
                     <div className={styles.entry}>
                         <div className={styles.nameContainer}>
-                            {/* <div className={styles.icon}><GlobeIcon/></div> */}
+                            {/* <div className={styles.icon}><ChainIcon/></div> */}
                             <a>
                                 <h3 className={styles.name}>{portfolio.entries[2].name}</h3>
                             </a>
+                            <div className={styles.links}>
+                                <a><div className={styles.icon}><ChainIcon/></div></a>
+                            </div>
                         </div>
-                        <p>{portfolio.credit.clientVenn1} {pageContent.grammar.para} {pageContent.grammar.o}<i> Conservation Strategy Fund</i>.</p>
-                        <div className={styles.links}>
-                            <a><div className={styles.icon}><GlobeIcon/></div></a>
-                        </div>
+                        <p>{portfolio.credit.clientVenn1} {pageContent.grammar.para} {pageContent.grammar.o}<i> Conservation Strategy Fund</i>.</p>                        
                     </div>
                     <div className={styles.entry}>
                         <div className={styles.nameContainer}>
-                            {/* <div className={styles.icon}><GlobeIcon/></div> */}
+                            {/* <div className={styles.icon}><ChainIcon/></div> */}
                             <a>
                                 <h3 className={styles.name}>oga imobiliaria</h3>
                             </a>
+                            <div className={styles.links}>
+                                <a><div className={styles.icon}><ChainIcon/></div></a>
+                            </div>
                         </div>
-                        <p>{portfolio.credit.clientVenn1} {pageContent.grammar.para} <i>Oga Imobiliaria</i>.</p>
-                        <div className={styles.links}>
-                            <a><div className={styles.icon}><GlobeIcon/></div></a>
-                        </div>
+                        <p>{portfolio.credit.clientVenn1} {pageContent.grammar.para} <i>Oga Imobiliaria</i>.</p>                        
                     </div>
                 </div>            
                 <div className={styles.line}></div>
