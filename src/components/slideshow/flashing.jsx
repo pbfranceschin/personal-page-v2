1e91e9507ac1e3ed61d978cdae4f25cf50568fb6
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-23 12:19:07 -0300

    new: flashing slideshow

---
+++ src/components/slideshow/flashing.jsx
@@ -0,0 +1,52 @@
+'use client'
+import { useEffect, useRef, useState } from "react";
+
+
+const wrap = (current, length) => {
+    if(current === length - 1)
+        return 0
+    return current + 1;
+}
+
+export function FlashingSlideshow ({ imageArray, width, aspectRatio }) {
+    const [current, setCurrent] = useState(0);
+    const [enter, setEnter] = useState(true);
+    const timeoutRef = useRef(null);
+    
+    useEffect(() => {
+        const nextSlide = () => {
+            setEnter(false);
+            timeoutRef.current = setTimeout(() => {
+                setCurrent(prev => wrap(prev, imageArray.length));
+                setEnter(true);
+            }, 200);
+        };
+
+        const slideInterval = setInterval(nextSlide, 2000);
+
+        return () => {
+            clearInterval(slideInterval);
+            clearTimeout(timeoutRef.current);
+        };
+    }, [imageArray.length]);
+
+    return (
+        <div style={{ 
+            aspectRatio: aspectRatio || 'auto',
+            width: width, 
+            height: 'auto', 
+            overflow: 'hidden',
+            transition: 'all 180ms ease-out',
+            opacity: enter ? 1 : 0
+        }}>
+            <img 
+            src={imageArray[current]} 
+            alt={`slide pic ${current}`}
+            style={{
+                height: '100%',
+                width: '100%'
+            }} 
+            />
+        </div>
+    )
+}
