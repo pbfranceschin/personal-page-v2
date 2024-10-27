5ca2c3c3a245192536a8da954029699f9a4d5022
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-27 17:04:17 -0300

    style: portfolio entries

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,15 +1,31 @@
 import styles from './portfolio.module.css';
-import { GitHubIcon, ChainIcon, CollapseIcon } from '../graphics/graphics';
-import { useCallback, useContext, useRef, useState } from 'react';
+import { GitHubIcon, ChainIcon, CollapseIcon, VennProtocolLogo, CSFLogo, OGALogo } from '../graphics/graphics';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../context/provider';
 import Collapse from '../collapse/collapse';
 import { FlashingSlideshow } from '../slideshow/flashing';
+import { PlainSlideshow } from '../slideshow/plain';
 
 export default function Portfolio() {
     const [openGroup, setOpenGroup] = useState('');
-    console.log('openGroup', openGroup)
+    const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+    useEffect(() => {
+        const checkTouchDevice = () => {
+            // Check for touch support
+            const hasTouchSupport = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+            setIsTouchDevice(hasTouchSupport);
+        };
+
+        checkTouchDevice();
+    }, []);
+
+    const handleEntryClick = useCallback((url) => {
+        if(isTouchDevice) return
+        window.open(url, '_blank', 'noopener noreferrer');
+    }, [isTouchDevice]);
     
-    const handleClick = useCallback((group) => {
+    const handleGroupClick = useCallback((group) => {
         if(openGroup === group) {
             setOpenGroup('');
         } else {
@@ -20,15 +36,15 @@ export default function Portfolio() {
     return (
         <div className={styles.page}>
             <div className={styles.pageContent}>
-                <Group title={'dapps'} isOpen={openGroup === 'dapps'} handleClick={() => handleClick('dapps')}>
-                    <DappsGroup isOpen={openGroup === 'dapps'} />
+                <Group title={'dapps'} isOpen={openGroup === 'dapps'} handleClick={() => handleGroupClick('dapps')}>
+                    <DappsGroup isOpen={openGroup === 'dapps'}  handleEntryClick={handleEntryClick}/>
                 </Group>
 
-                <Group isOpen={openGroup === 'apps'} title={'web apps'} handleClick={() => handleClick('apps')}>
+                <Group isOpen={openGroup === 'apps'} title={'web apps'} handleClick={() => handleGroupClick('apps')}>
                     <WebAppsGroup isOpen={openGroup === 'apps'} />
                 </Group>
 
-                <Group isOpen={openGroup === 'pages'} title={'web pages'} handleClick={() => handleClick('pages')}>
+                <Group isOpen={openGroup === 'pages'} title={'web pages'} handleClick={() => handleGroupClick('pages')}>
                     <WebPagesGroup isOpen={openGroup === 'pages'} />
                 </Group>
             </div>
@@ -37,6 +53,7 @@ export default function Portfolio() {
 }
 
 function Group ({ title, isOpen, handleClick, children }) {
+    
     return (
         <div className={styles.group}>
             <div className={styles.titlebox} onClick={handleClick}>
@@ -52,7 +69,7 @@ function Group ({ title, isOpen, handleClick, children }) {
     )
 }
 
-function DappsGroup ({ isOpen }) {
+function DappsGroup ({ isOpen, handleEntryClick }) {
     const { pageContent } = useContext(AppContext);
     const { portfolio } = pageContent;
     const { dapps } = portfolio;
@@ -60,51 +77,49 @@ function DappsGroup ({ isOpen }) {
     return (
         <Collapse isOpen={isOpen}>
             <div className={styles.textbox}>
-                <div className={styles.venn}>
-                    <div className={styles.entryInfo}>
-                        <div className={styles.nameContainer}>                
-                            <a>
-                                <h3 className={styles.name}>venn protocol</h3>
-                            </a>
-                            <div className={styles.links}>
-                            <a>
-                                <div className={styles.icon}
-                                style={{ padding: '1px'}}
-                                >
-                                    <GitHubIcon/>                            
-                                </div>
-                            </a>
-                            <a>
-                                <div className={`${styles.icon} ${styles.iconRounded}`}>
-                                    <ChainIcon/>
-                                </div>
-                            </a>                                
+                <div className={styles.entry}>
+                    <div className={styles.nameContainer}>                
+                            <h3 className={styles.name}>venn protocol</h3>
+                            <div className={styles.linksWrapper}>
+                                <a className={styles.links}>
+                                    Github
+                                </a>
+                                <a className={styles.links}>
+                                    App
+                                </a>                                
+                            </div>
+                    </div>
+                    <div className={styles.entryInfo}>                                            
+                        <div className={styles.overlaylogo} 
+                        style={{ padding: '2rem' }}
+                        onClick={() => handleEntryClick('https://venn-procol.vercel.app')}
+                        >
+                            <VennProtocolLogo/>
                         </div>
-                        </div>
+                        <PlainSlideshow 
+                        width={'100%'} 
+                        aspectRatio={1920 / 1080} 
+                        imageArray={[
+                            '/img/venn-protocol/venn_landing.png',
+                            '/img/venn-protocol/venn_mktplace.png',
+                            '/img/venn-protocol/venn_dashboard.png'
+
+                        ]}
+                        />
                         <p>
                             {dapps[0].description[0]}
                         </p>
                         <p>
                             {portfolio.credit.selfVenn}
                         </p>                
-                    </div>
-                    <FlashingSlideshow 
-                    width={'100%'} 
-                    aspectRatio={1920 / 1080} 
-                    imageArray={[
-                        '/img/venn-protocol/venn_landing.png',
-                        '/img/venn-protocol/venn_mktplace.png',
-                        '/img/venn-protocol/venn_dashboard.png'
-
-                    ]}
-                    />
+                    </div>                    
                 </div>
             </div>
         </Collapse>
     )
 }
 
-function WebAppsGroup ({ isOpen }) {
+function WebAppsGroup ({ isOpen, handleEntryClick }) {
     const { pageContent } = useContext(AppContext);
     const { portfolio } = pageContent;
     const { apps } = portfolio;
@@ -112,27 +127,33 @@ function WebAppsGroup ({ isOpen }) {
     return (
         <Collapse isOpen={isOpen}>
             <div className={styles.textbox}>
-                <div className={styles.entryInfo}>
+                <div className={styles.entry}>
                     <div className={styles.nameContainer}>
-                        <a>
-                            <h3 className={styles.name}>{apps[0].name}</h3>
-                        </a>
-                        <div className={styles.links}>
-                            <a><div className={`${styles.icon} ${styles.iconRounded}`}><ChainIcon/></div></a>
+                            <h3 className={styles.name}>{apps[0].name}</h3>                        
+                            <a className={styles.links}>App</a>
                         </div>
+                    <div className={styles.entryInfo}>
+                        <div className={styles.overlaylogo} 
+                        style={{ padding: '3rem'}}
+                        onClick={() => handleEntryClick('https://deforestationcalculator.conservation-strategy.org/')}
+                        >
+                            <CSFLogo/>
+                        </div>
+                        <img 
+                        src='/img/csf/deforestation-home.png' 
+                        alt='deforestation_calculator-home' 
+                        style={{ width: '100%', height: 'auto' }}
+                        />
+                        <p>{apps[0].description[0]}</p>
+                        <p>{portfolio.credit.clientVenn1}. <span style={{ textTransform: 'capitalize'}}>{pageContent.words.project}</span> {pageContent.grammar.do}<i> Conservation Strategy Fund</i>.</p>
                     </div>
-                    <p>{apps[0].description[0]}</p>
-                    <p>{portfolio.credit.clientVenn1}. <span style={{ textTransform: 'capitalize'}}>{pageContent.words.project}</span> {pageContent.grammar.do}<i> Conservation Strategy Fund</i>.</p>
-                    {/* <div className={styles.links}>
-                        <a><div className={`${styles.icon} ${styles.iconRounded}`}><ChainIcon/></div></a>
-                    </div> */}
                 </div>                    
             </div>
         </Collapse>
     )
 }
 
-function WebPagesGroup ({ isOpen }) {
+function WebPagesGroup ({ isOpen, handleEntryClick }) {
     const { pageContent } = useContext(AppContext);
     const { portfolio } = pageContent;
     const { pages } = portfolio;
@@ -140,29 +161,42 @@ function WebPagesGroup ({ isOpen }) {
     return (
         <Collapse isOpen={isOpen}>
             <div className={styles.textbox}>
-                <div className={styles.entryInfo}>
-                    <div className={styles.nameContainer}>
-                        {/* <div className={`${styles.icon} ${styles.iconRounded}`}><ChainIcon/></div> */}
-                        <a>
-                            <h3 className={styles.name}>{pages[0].name}</h3>
-                        </a>
-                        <div className={styles.links}>
-                            <a><div className={`${styles.icon} ${styles.iconRounded}`}><ChainIcon/></div></a>
-                        </div>
+                <div className={styles.entry}>
+                    <div className={styles.nameContainer}>                        
+                        <h3 className={styles.name}>{pages[0].name}</h3>
+                        <a className={styles.links}>Website</a>
                     </div>
-                    <p>{portfolio.credit.clientSolo} {pageContent.grammar.para} {pageContent.grammar.o}<i> Conservation Strategy Fund</i>.</p>
+                    <div className={styles.entryInfo}>     
+                        <div className={styles.overlaylogo} 
+                        style={{ padding: '3rem'}}
+                        onClick={() => handleEntryClick('https://calculators.conservation-strategy.org/')}
+                        >
+                            <CSFLogo/>
+                        </div>
+                        <img 
+                        src='/img/csf/hub.png' 
+                        alt='calculator hub'
+                        style={{ width: '100%' , height: 'auto' }}
+                        />
+                        <p>{portfolio.credit.clientSolo} {pageContent.grammar.para} {pageContent.grammar.o}<i> Conservation Strategy Fund</i>.</p>
+                    </div>
                 </div>
-                <div className={styles.entryInfo}>
+                <div className={styles.entry}>
                     <div className={styles.nameContainer}>
-                        {/* <div className={`${styles.icon} ${styles.iconRounded}`}><ChainIcon/></div> */}
-                        <a>
-                            <h3 className={styles.name}>oga imobiliaria</h3>
-                        </a>
-                        <div className={styles.links}>
-                            <a><div className={`${styles.icon} ${styles.iconRounded}`}><ChainIcon/></div></a>
-                        </div>
+                            <h3 className={styles.name}>oga imobiliaria</h3>                            
+                            <a className={styles.links}>Website</a>
                     </div>
-                    <p>{portfolio.credit.clientVenn1} {pageContent.grammar.para} <i>Oga Imobiliaria</i>.</p>                        
+                    <div className={styles.entryInfo}>
+                        <div className={styles.overlaylogo} style={{ padding: '0 1rem' }}>
+                            <OGALogo/>
+                        </div>
+                        <img
+                        src='/img/csf/deforestation-home.png'           
+                        alt='OGA homepage'
+                        style={{ width: '100%', height: 'auto' }}
+                        />
+                        <p>{portfolio.credit.clientVenn1} {pageContent.grammar.para} <i>Oga Imobiliaria</i>.</p>                        
+                    </div>
                 </div>
             </div>
         </Collapse>            
